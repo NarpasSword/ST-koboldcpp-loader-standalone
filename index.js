b7e96ae2c22c5c4e10e2694a4204bd31b5cadf2f
wolf <wolf@git>
2024-10-22 18:11:02 -0700

    numbers only

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function onKoboldURLChanged() {
 function onKoboldContextChanged() {
     let v = $(this).val();
     if (v == "") return;
-    if (!(v===(""+parseInt(v)))) {
+    if (isNaN(v)) {
         v = 8;
         $(this).val(8);
     }
